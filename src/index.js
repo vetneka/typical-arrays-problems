813fe117a6c6c6582bd326d4f9eb7eec7735643f
vetneka <vetneka@yandex.ru>
2021-02-19 10:00:54 +0500

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length === 0) return 0;
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0) return 0;
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length === 0) return 0;
+  return array.reduce((sum, item) => sum + item, 0) / array.length;
 }
